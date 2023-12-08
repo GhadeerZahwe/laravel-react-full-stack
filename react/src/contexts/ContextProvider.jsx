47b72fd7deb1f58b4e80145b5d73e9c5db477212
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-09 00:20:12 +0200

    state context

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -1,23 +1,35 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
  const StateContext = createContext({defaultValue:{
-currentUser:null,
-token:null
+user:null,
+token:null,
+setUser:()=>{},
+setToken:()=>{}
  }})
 
  export const ContextProvider = (children) => {
     const [user, setUser] = useState({initialState:{}});
-    const [token, _setToken] = useState({initialState:null});
+    const [token, _setToken] = useState(localStorage.getItem({key:'ACCESS_TOKEN'}));
 
 const setToken = (token) => {
     _setToken(token);
+    if(token){
+        localStorage.setItem('ACCESS_TOKEN',token);
+    } else{
+        localStorage.removeItem({key:'ACCESS_TOKEN'});
+    }
 }
 
     return(
         <StateContext.Provider Value={{
-
+         user,
+         token,
+         setUser,
+         setToken
         }}>
-
+        {children}
         </StateContext.Provider>
     )
  }
+
+ export const useStateContext=()=>useContext(StateContext)
