9ef5bebb543ce2934b447dd7eac52df3ac6eca32
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-08 22:27:34 +0200

    add children components

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,15 +1,36 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './views/Login';
-import Signup from './views/Signup';
-import NotFound from './views/NotFound';
-import Users from './views/Users';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import NotFound from "./views/NotFound";
+import Users from "./views/Users";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
 
 const router = (
   <Router>
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/users" element={<Users />} />
+      <Route
+        path="/"
+        element={
+          <DefaultLayout>
+            <Routes>
+              <Route path="/users" element={<Users />} />
+            </Routes>
+          </DefaultLayout>
+        }
+      />
+      <Route
+        path="/guest"
+        element={
+          <GuestLayout>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </GuestLayout>
+        }
+      />
+
       <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>
