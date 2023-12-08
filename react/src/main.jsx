1beaa6922e10ab111a608707f2462c0114dd9c98
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-08 21:53:47 +0200

    fix routing

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './views/Login';
+import Signup from './views/Signup';
+import NotFound from './views/NotFound';
+import Users from './views/Users';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,20 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
-import Login from "./views/Login";
-import Signup from "./views/Signup";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './views/Login';
+import Signup from './views/Signup';
+import NotFound from './views/NotFound';
+import Users from './views/Users';
 
-const router =createBrowserRouter(routes[
-    {
-        path:'/login',
-        element: <Login/>
-    },
-    {
-        path:'/signup',
-        element: <Signup/>
-    },
-    {
-        path:'/users',
-        element: <Users/>
-    }
+const router = (
+  <Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/users" element={<Users />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Router>
+);
 
-]);
 export default router;
