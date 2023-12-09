591ee8d9881a7900522a0ef00c981470f6727e41
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-09 18:43:40 +0200

    logout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -11,19 +11,30 @@ export default function DefaultLayout() {
 
   const onLogout = (ev) => {
     ev.preventDefault();
-  };
+  axiosClient.post('/logout')
+  .then(()=>{
+    setUser({})
+      setToken(null)
+  })
+};
 
+useEffect(()=>{
+  axiosClient.get('/user')
+  .then(({data})=>{
+    setUser(data)
+  })
+},[])
   return (
     <div id="defaultLayout">
       <aside>
         <Link to="/dashboard"> Dashboard</Link>
-        <Link to="/users"> User Info</Link>
+        <Link to="/users"> Users</Link>
       </aside>
       <div className="content">
         <header>
           <div>Header</div>
           <div>
-            {user.name}
+            {user.name}  &nbsp; &nbsp;
             <a onClick={onLogout} className="btn-logout" href="#">
               Logout
             </a>
